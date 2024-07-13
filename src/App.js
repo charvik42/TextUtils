ed91b2a6ce3f602fbbda05c637459b6f3827ff3c
Charvi <charvikapoor1996@gmail.com>
2024-07-14 02:07:26 +0530

    This was the first commit to tell this idiot git that everything in this folder is in fact, important.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar title="Set Title Here" aboutText="About TextUtils" />
+    </>
+  );
+}
+
+export default App;
