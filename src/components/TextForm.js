c05fa5bb213c719d002d431c6b6342d732ee8b50
Charvi <charvikapoor1996@gmail.com>
2024-07-15 19:56:35 +0530

    Added text summary feature

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,17 +17,27 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
 
     return (
-        <div>
+        <>        
+            <div className="container">
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control" value= {text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
-            <button className="btn btn-primary" onClick={handleDownClick}>Convert to lowercase</button>
-        </div>
+            <button className="btn btn-success mx-1" onClick={handleUpClick}>Convert to uppercase</button>
+            <button className="btn btn-success mx-1" onClick={handleDownClick}>Convert to lowercase</button>
+            </div>
+            <div className="container my-3">
+                <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008*text.split(" ").length} minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
+
   )
 }
 
