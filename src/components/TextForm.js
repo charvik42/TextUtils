3472858b3dc06f7adc824d10f6e6354598373a41
Charvi <charvikapoor1996@gmail.com>
2024-07-17 21:03:39 +0530

    Added copy text and remove extra space buttons

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -49,6 +49,17 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+    const handleCopy = () =>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () =>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
     return (
         <>        
             <div className="container">
@@ -61,6 +72,8 @@ export default function TextForm(props) {
             <button className="btn btn-success mx-1" onClick={handleClearText}>Clear Text</button>
             <button className="btn btn-success mx-1" onClick={handleFirst}>Convert first letter to capital</button>
             <button className="btn btn-success mx-1" onClick={handleBold}>Convert to bold/italics</button>
+            <button className="btn btn-success mx-1" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-success mx-1 my-2" onClick={handleExtraSpaces}>Remove extra spaces</button>
             </div>
             <div className="container my-3">
                 <h2>Your Text Summary</h2>
